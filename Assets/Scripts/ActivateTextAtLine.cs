daffe9dd32f36cae111df1f97382f46bb7d49544
Sarah Slovak <>
2021-04-29 22:21:26 -0400

    Asset meta's and file changes

---
+++ Assets/Scripts/ActivateTextAtLine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateTextAtLine : MonoBehaviour
+{
+    public TextAsset theText;
+
+    public int startLine;
+    public int endLine;
+
+    public TextBoxManager theTextBox;
+
+    public bool destroyWhenActivated;
+    public bool requireButtonPress;
+    private bool waitForPress;
+
+    //CHANGE THIS SO IT'S WHEN YOU ENTER THE SHOUT ZONE, NOT WHEN YOU CLICK A BUTTON
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
