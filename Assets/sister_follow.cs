3a70303c5cadb7bfb1d132d28a97d1657d08ddd6
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2021-05-01 05:31:03 -0400

    sister_follow

---
+++ Assets/sister_follow.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sister_follow : MonoBehaviour
+{
+    public int ray_distance;
+    public static Animator animator;
+    public float speed;
+    public GameObject player;
+    private SpriteRenderer mySpriteRenderer;
+    public static int move_vertical;
+    public static int move_horizontal;
+
+    public LayerMask barrier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        PlayerTriggers.sister_follow = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left, ray_distance, barrier);
+        Vector3 localPosition = player.transform.position - transform.position;
+
+        localPosition = localPosition.normalized; // The normalized direction in LOCAL space
+        transform.Translate(localPosition.x * Time.deltaTime * speed * move_vertical, move_horizontal * localPosition.y * Time.deltaTime * speed, localPosition.z * Time.deltaTime * speed);
+      
+        if (Vector2.Distance(player.transform.position, transform.position) < 20)
+        {
+            move_vertical = 0;
+            move_horizontal = 0;
+
+        }
+        else
+        {
+
+            if (hit.collider != null)
+            {
+
+                move_vertical = 0;
+                move_horizontal = 4;
+
+
+            }
+            else
+            {
+                move_vertical = 2;
+                move_horizontal = 2;
+            }
+        }
+        if (player.transform.position.x > transform.position.x)
+        {
+            mySpriteRenderer.flipX = false;
+        }
+        else
+        {
+            mySpriteRenderer.flipX = true;
+        }
+
+
+
+    }
+   
+}
